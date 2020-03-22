125b48fe79c0d373cf231a24e2489e67eb7b5d30
musta7188 <musta7188@hotmail.it>
2020-03-22 17:06:36 +0000

    game serializer and method index show game done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :games
-  resources :cards
-  resources :players
+  resources :games, only: [:show, :index]
+  # resources :cards
+  # resources :players
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
