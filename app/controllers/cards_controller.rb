fc4f27fea4f3c8d2cadaea8a874a92caafd78b55
musta7188 <musta7188@hotmail.it>
2020-03-27 12:48:01 +0000

    added more card

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,7 +1,7 @@
 class CardsController < ApplicationController
 
   def index 
-    cards = Card.all.shuffle[0..5].take(6)
+    cards = Card.all.shuffle[0..6].take(6)
     render json: cards, only: [:id, :image1, :image2, :word, :points]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,10 @@ Card.create(image1: "https://www.azocleantech.com/images/Article_Images/ImageFor
 Card.create(image1: "https://cdn.britannica.com/55/174255-050-526314B6/brown-Guernsey-cow.jpg", image2:"https://i.ytimg.com/vi/3wLLgJ_a7Rs/maxresdefault.jpg", word: "cowboy", points: 15)
 Card.create(image1: "https://townsquare.media/site/812/files/2019/06/nas.jpg?w=980&q=75", image2:"https://bellacaledonia.org.uk/wp-content/uploads/2018/02/letter_a_by_hillygon-d9h8c6a.jpg", word: "nasa", points: 10)
 Card.create(image1: "https://thumbs.dreamstime.com/b/fresh-honey-25578978.jpg", image2:"https://nineplanets.org/wp-content/uploads/2019/09/moon.png", word: "honeymoon", points: 9)
+
+
+Card.create(image1: "https://media.istockphoto.com/photos/open-female-hand-to-hold-something-picture-id488259893?k=6&m=488259893&s=612x612&w=0&h=PPgs4-1KAothx46TEcdReqKapIdmCb_85IfaW1AdzNM=", image2:"https://cdn3.whatculture.com/images/2019/01/66270bdf7bc1140f-600x338.jpg", word: "armwrestle", points: 9)
+Card.create(image1: "https://www.therange.co.uk/_m5/7/8/1498642143_782.jpg", image2:"https://vignette.wikia.nocookie.net/btb/images/6/68/BobSavestheHedgehogs105.png/revision/latest?cb=20190806061908", word: "spongebob", points: 5)
+Card.create(image1: "https://images.unsplash.com/photo-1517594422361-5eeb8ae275a9?ixlib=rb-1.2.1&w=1000&q=80", image2:"https://researchmatters.in/sites/default/files/flies.jpg", word: "firefly", points: 7)
+Card.create(image1: "https://www.ikea.com/gb/en/images/products/ikea-365--frying-pan__0605691_PE681809_S5.JPG", image2:"https://www.oetker.co.uk/Recipe/Recipes/oetker.co.uk/uk-en/baking/image-thumb__73438__RecipeDetail/rainbow-sprinkle-pinata-cake.jpg", word: "pancake", points: 5)
+Card.create(image1: "https://cnet4.cbsistatic.com/img/AXwQBJb1aVW35t86aTpUZFoote8=/940x0/2017/09/20/32a94ec6-03a1-4225-b799-cf2e4f3d127f/cree-floodlight-4.jpg", image2:"https://cdn.vox-cdn.com/thumbor/XUdUk_rKJ8WDTd0zksnnicYDwbY=/0x0:3760x2500/1200x800/filters:focal(1580x950:2180x1550)/cdn.vox-cdn.com/uploads/chorus_image/image/65135516/shutterstock_349464740.0.jpg", word: "lighthouse", points: 5)
